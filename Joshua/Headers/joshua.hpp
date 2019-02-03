170367ed4868128e38e4c671aeb71557aaf5b730
Reda Borchardt <redaborchardt@gmail.com>
2019-02-03 17:23:15 +0200

    Source

---
+++ Joshua/Headers/joshua.hpp
@@ -0,0 +1,22 @@
+// Preprocessor Directives
+#ifndef JOSHUA
+#define JOSHUA
+#pragma once
+
+// System Headers
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <btBulletDynamicsCommon.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Add to one source file:  #define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+// Define Some Constants
+const int mWidth = 1280;
+const int mHeight = 800;
+
+#endif //~ Joshua Header
