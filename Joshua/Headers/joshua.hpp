399b47ae333f0d2f355a4895d0c2afd19817e592
Reda Borchardt <redaborchardt@gmail.com>
2019-02-16 11:43:37 +0200

    Swapped GLAD for GL3W

--- Joshua/Headers/joshua.hpp
+++ Joshua/Headers/joshua.hpp
@@ -8,7 +8,6 @@
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
 #include <btBulletDynamicsCommon.h>
-#include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
